19f26aa9373ed8478aba77af20146f831858d5da
Piyush <piyush.0224kp@gmail.com>
2025-02-13 12:23:09 +0530

    fix chart data points

--- react-with-vite/src/Utils/helper.ts
+++ react-with-vite/src/Utils/helper.ts
@@ -2,20 +2,20 @@
 // Data Transformation Function
 export const transformData = (dummyData: any, stock: string) => {
     return dummyData[stock].map((item: any) => {
-        const [CE_ShortCovering, CE_ShortBuildup, CE_LongBuildup, CE_LongUnwinding] =
+        const [CE_ShortBuildup, CE_LongBuildup, CE_ShortCovering, CE_LongUnwinding] =
             item?.CE?.split(",").map(Number) || [0, 0, 0, 0];
-        const [PE_ShortCovering, PE_ShortBuildup, PE_LongBuildUp, PE_LongUnwinding] =
+        const [PE_ShortBuildup, PE_LongBuildUp, PE_ShortCovering, PE_LongUnwinding] =
             item?.PE?.split(",").map(Number) || [0, 0, 0, 0];
 
         return {
             time: item?.time?.split(":").slice(0, 2).join(":") || "", // Removing seconds and milliseconds
-            CE_ShortCovering,
             CE_ShortBuildup,
             CE_LongBuildup,
+            CE_ShortCovering,
             CE_LongUnwinding,
-            PE_ShortCovering,
             PE_ShortBuildup,
             PE_LongBuildUp,
+            PE_ShortCovering,
             PE_LongUnwinding,
         };
     });
